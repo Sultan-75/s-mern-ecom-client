ac3aced913278a7205553ba54c1c83e86e21d6cb
Your Name <you@example.com>
2021-06-11 01:34:12 -0800

    urlConfig changes

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,13 @@
-export const api = "http://localhost:2000/api";
+const baseUrl = "https://s-mern-ecom-server.herokuapp.com";
+
+export const api = `${baseUrl}/api`;
 
 export const generatePublicUrl = (fileName) => {
-  return `http://localhost:2000/public/${fileName}`;
+  return `${baseUrl}/public/${fileName}`;
 };
+
+// export const api = "http://localhost:2000/api";
+
+// export const generatePublicUrl = (fileName) => {
+//   return `http://localhost:2000/public/${fileName}`;
+// };
