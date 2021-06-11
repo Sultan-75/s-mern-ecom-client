9e583b6c125842642ac6766b10db4d3e82ede14b
Your Name <you@example.com>
2021-06-11 01:05:23 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./containers/HomePage";
+import ProductListPage from "./containers/productListPage";
+import { useDispatch, useSelector } from "react-redux";
+import { isUserLoggedIn, updateCart } from "./actions";
+import ProductDetailsPage from "./containers/ProductDetailsPage";
+import CartPage from "./containers/CartPage";
+import CheckoutPage from "./containers/CheckoutPage";
+import OrderPage from "./containers/OrderPage";
+import OrderDetailsPage from "./containers/OrderDetailsPage";
+
+function App() {
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!auth.authenticate) {
+      dispatch(isUserLoggedIn());
+    }
+  }, [auth.authenticate]);
+
+  useEffect(() => {
+    console.log("App.js - updateCart");
+    dispatch(updateCart());
+  }, [auth.authenticate]);
+  //
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/cart" component={CartPage} />
+          <Route path="/checkout" component={CheckoutPage} />
+          <Route path="/account/orders" component={OrderPage} />
+          <Route path="/order_details/:orderId" component={OrderDetailsPage} />
+          <Route
+            path="/:productSlug/:productId/p"
+            component={ProductDetailsPage}
+          />
+          <Route path="/:slug" component={ProductListPage} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
